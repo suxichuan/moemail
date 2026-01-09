c0c0d304c35bc175bd8ccd2f96ca556abd5ffe29
suxichuan <115853887+suxichuan@users.noreply.github.com>
2026-01-09 22:29:47 +0800

    Update fonts.ts

--- app/fonts.ts
+++ app/fonts.ts
@@ -1,7 +1,7 @@
 import localFont from 'next/font/local'
 
 export const zpix = localFont({
-  src: '../public/fonts/HanYiCuYuanJian-1.ttf',
+  src: '../public/fonts/ZhouZiFangTi241010-TTF-2.ttf',
   variable: '--font-zpix',
   display: 'swap',
 }) 
